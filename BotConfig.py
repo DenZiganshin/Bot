51a07c898263659344ef4c772d2117762e9b9d6b
Кожемяко Сергей <kosa@nita.ru>
2023-11-14 21:44:06 +0300

    Task done

---
+++ BotConfig.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+class BotConfig:
+
+    def __init__(self, config_path = Path('~').expanduser() / ".balabol_bot/config.json"):
+        self.config_path = config_path
+        self.__read_config__()
+
+    def __read_config__(self):
+        with open(self.config_path) as f:
+            json_cfg = json.load(f)
+            self.database_row_limit = json_cfg["database_row_limit"]
+            self.phrases_in_bot_message_count = json_cfg["phrases_in_bot_message_count"]
+            self.inbound_phrase_length_limit = json_cfg["inbound_phrase_length_limit"]
+            self.message_initiation_frequency = json_cfg["message_initiation_frequency"]
+            return
+        raise Exception(f"Invalid file path {self.config_path}")
+
+
